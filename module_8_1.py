1dffad381dc136dc0474ac86e1bffd70a33ff341
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-09-29 12:40:35 +0300

    module_8

--- module_8_1.py
+++ module_8_1.py
@@ -8,6 +8,7 @@ def add_everything_up(a, b):
         print(f"{a}{b}")
 
 
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+add_everything_up(123.456, 'строка')
+add_everything_up('яблоко', 4215)
+add_everything_up(123.456, 7)
+
